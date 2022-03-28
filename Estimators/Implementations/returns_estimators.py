6ab0d924cd64c9d801ce5a97700087e106a1f2ff
Danny816123 <77084035+Danny816123@users.noreply.github.com>
2022-03-28 17:16:49 +0430

    D

--- Estimators/Implementations/returns_estimators.py
+++ Estimators/Implementations/returns_estimators.py
@@ -1,6 +1,7 @@
+import warnings
+import numpy as np
+import pandas as pd
 
-
-# pylint: disable=missing-module-docstring
 class ReturnsEstimators:
     """
     This class contains methods for estimating expected returns. A good estimation of the asset expected returns is very important
@@ -12,8 +13,6 @@ class ReturnsEstimators:
         """
         Initialize
         """
-        
-        print("Hello World!")
 
 
         pass
@@ -22,21 +21,26 @@ class ReturnsEstimators:
     def calculate_mean_historical_returns(asset_prices, resample_by=None, frequency=252):
         """
         Calculates the annualised mean historical returns from asset price data.
-
         :param asset_prices: (pd.DataFrame) Asset price data
         :param resample_by: (str) Period to resample data ['D','W','M' etc.] None for no resampling
         :param frequency: (int) Average number of observations per year
         :return: (pd.Series) Annualized mean historical returns per asset
         """
 
-        pass
+        if not isinstance(asset_prices, pd.DataFrame):
+                warnings.warn("prices are not in a dataframe", RuntimeWarning)
+                asset_prices = pd.DataFrame(asset_prices)
+        
+        returns = ReturnsEstimators().calculate_returns(asset_prices, resample_by)
+
+        return returns.mean() * frequency
+
 
     @staticmethod
     def calculate_exponential_historical_returns(asset_prices, resample_by=None, frequency=252, span=500):
         """
         Calculates the exponentially-weighted annualized mean of historical returns, giving
         higher weight to more recent data.
-
         :param asset_prices: (pd.DataFrame) Asset price data
         :param resample_by: (str) Period to resample data ['D','W','M' etc.] None for no resampling
         :param frequency: (int) Average number of observations per year
@@ -44,16 +48,24 @@ class ReturnsEstimators:
         :return: (pd.Series) Exponentially-weighted mean of historical returns
         """
 
-        pass
+        if not isinstance(asset_prices, pd.DataFrame):
+                warnings.warn("prices are not in a dataframe", RuntimeWarning)
+                asset_prices = pd.DataFrame(asset_prices)
+        
+        returns = ReturnsEstimators().calculate_returns(asset_prices, resample_by)
+
+        return returns.ewm(span=span).mean().iloc[-1] * frequency
 
     @staticmethod
     def calculate_returns(asset_prices, resample_by=None):
         """
         Calculates a dataframe of returns from a dataframe of prices.
-
         :param asset_prices: (pd.DataFrame) Historical asset prices
         :param resample_by: (str) Period to resample data ['D','W','M' etc.] None for no resampling
         :return: (pd.DataFrame) Returns per asset
         """
 
-        pass
+        if resample_by != None:
+            asset_prices = asset_prices.resample(resample_by).sum()
+
+        return asset_prices.pct_change().dropna(how="all")
